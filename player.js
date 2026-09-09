c094be47da6a25bc2dde9351ecb19a64046f1def
Ekansh Khatri <ekanshkhatri@gmail.com>
2026-09-09 23:29:51 +0530

    Initial commit

--- player.js
+++ player.js
@@ -10,3 +10,30 @@ const songs = fs
 let selected = 0;
 let currentSong = null;
 let progressTimer = null;
+
+
+// Show the song list
+function showSongs() {
+  console.clear();
+
+  console.log("🎶 Welcome to the Songs App 🎶\n");
+
+  for (let i = 0; i < songs.length; i++) {
+
+    const songName = songs[i].split(".")[0];
+
+    if (selected === i) {
+      console.log(`-> ${i + 1}: ${songName}`);
+    } else {
+      console.log(`   ${i + 1}: ${songName}`);
+    }
+  }
+
+  console.log("\n⬆️⬇️ Use arrow keys to select");
+  console.log("↵ Press Enter to play");
+  console.log("P Press P to pause");
+  console.log("R Press R to resume");
+  console.log("Q Press Q to quit");
+}
+
+showSongs();
